ea0807cb75452ae9f7e0a85b6028790cbba7fd43
Sangita Halder <sangitah652@gmail.com>
2026-08-06 21:33:31 +0530

    add cookie-parser dependency and update server entry point

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,19 @@
-import http from "http";
 import dotenv from "dotenv";
-import connectDB from ".db/index.js";
-import {app} from "./app.js";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:"./.env"
-})
+    path: "./.env"
+});
 
 connectDB()
-.then(()=>{
-    const PORT=process.env.PORT || 5000;
-    app.listen(PORT,()=>{
-        console.log(`Server is running on port ${PORT}`);
+    .then(() => {
+        const PORT = process.env.PORT || 5000;
+        app.listen(PORT, () => {
+            console.log(`⚙️  Server is running at http://localhost:${PORT}`);
+        });
     })
-})
-.catch((error)=>{
-    console.error("Error connecting to the database:", error);
-    process.exit(1);
-})
+    .catch((err) => {
+        console.error("❌ MONGODB connection failed: ", err);
+        process.exit(1);
+    });
